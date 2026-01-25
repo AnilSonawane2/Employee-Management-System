12fb4ad3692875a66e2b7105a9212f54d3d62275
Anil Sonawane <anil.sonawane2003@gmail.com>
2026-01-25 19:12:10 +0530

    Will add verify fn

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom"
 
 import Login from "./pages/Login"
 import AdminDashboard from "./pages/AdminDashboard"
+import EmployeeDashboard from "./pages/EmployeeDashboard"
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
         <Route path="/" element={<Navigate to="/admin-dashboard"/>}></Route>
         <Route path="/login" element={<Login/>}></Route>
         <Route path="/admin-dashboard" element={<AdminDashboard/>}></Route>
+        <Route path="/employee-dashboard" element={<EmployeeDashboard/>}></Route>  
       </Routes>
     </BrowserRouter>
   )
--- Client/src/context/authContext.jsx
+++ Client/src/context/authContext.jsx
@@ -1,15 +1,34 @@
-import React, { createContext, useContext, useState } from 'react'
+import React, { createContext, useContext, useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const userContext = createContext()
 const authContext = ({children}) => {
 
     const [user, setUser] = useState(null)
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        const verifyUser = async () => {
+            try{
+                const response = await axios.get('http://localhost:8000/api/auth/verify')
+                if(response.data.success){
+                    setUser(response.data.user)
+                }
+            }
+            catch(error){
+                if(error.response && !error.response.data.error){
+                    navigate('/login')
+                }
+            }
+        }
+        verifyUser() // <-- to verify user
+    })
     
     const login = (user) => {
         setUser(user)
     }
 
-    const logout = () => {
+    const logout = () => {  // whenever we logout remove the token from localstorage
         setUser(null)
         localStorage.removeItem("token")
     }
